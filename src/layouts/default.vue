d60bb085885161197e0d612878abfabc675277f0
AClon314 <1620340682@qq.com>
2024-03-31 18:34:18 +0800

    windows bar

--- src/layouts/default.vue
+++ src/layouts/default.vue
@@ -1,16 +1,54 @@
 <template>
   <v-app>
+    <v-app-bar
+      color="primary"
+      density="compact"
+      :height="isPC ? 50 : undefined"
+    >
+      <v-app-bar-nav-icon></v-app-bar-nav-icon>
 
-    
+      <v-app-bar-title class="min-content" >Photos</v-app-bar-title>
+      <v-spacer class="fill-height" data-tauri-drag-region></v-spacer>
 
-    <v-main>
+      <v-btn icon="mdi-dots-vertical"></v-btn>
+
+      <v-btn
+        icon="mdi-minus"
+        variant="text"
+        @click="() => getCurrent().minimize()"
+        v-if="isPC"
+      ></v-btn>
+      <v-btn
+        class="ms-2"
+        icon="mdi-checkbox-blank-outline"
+        variant="text"
+        @click="() => getCurrent().toggleMaximize()"
+        v-if="isPC"
+      ></v-btn>
+      <v-btn
+        class="ms-2"
+        icon="mdi-close"
+        variant="text"
+        @click="() => getCurrent().close()"
+        v-if="isPC"
+      ></v-btn>
+    </v-app-bar>
+
+    <v-main scrollable>
       <router-view />
     </v-main>
 
-    <AppFooter />
+    <!-- <AppFooter /> -->
   </v-app>
 </template>
 
 <script lang="ts" setup>
-  //
+import { getCurrent } from "@tauri-apps/api/window";
+
+const isPC = navigator.userAgent.indexOf("Mobile") == -1;
 </script>
+<style scoped>
+.v-toolbar-title{
+  flex:none;
+}
+</style>
--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,7 +1,13 @@
 <template>
-  <HelloWorld />
+  <v-container>
+    <v-row>
+      <v-col v-for="n in 24" :key="n" cols="4">
+        <v-card height="200"></v-card>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
 <script lang="ts" setup>
-  //
+//
 </script>
