a48d845194bd0f7874961c9249f619899c87d23a
AClon314 <30747832+AClon314@users.noreply.github.com>
2024-05-10 20:53:42 +0800

    new tempalte init

---
+++ src-tauri/gen/android/app/src/main/java/com/tauri/weather_app/MainActivity.kt
@@ -0,0 +1,3 @@
+package com.tauri.weather_app
+
+class MainActivity : TauriActivity()
---
+++ src/components/weather.vue
@@ -0,0 +1,12 @@
+<script setup>
+
+</script>
+
+<template>
+  <div class="row">
+    <h2>Weather</h2>
+    <p>Click the button to get the weather</p>
+    <button @click="getWeather">Get Weather</button>
+    <p>{{ weather }}</p>
+  </div>
+</template>
